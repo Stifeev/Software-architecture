a406737222a94fd99d2eb216d05cf76ae5a775eb
Stifeev <55450005+Stifeev@users.noreply.github.com>
2022-03-23 00:55:35 +0300

    Add files via upload

---
+++ ЛР1/Код/build/CMakeFiles/client.exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.exe.dir/client.cpp.o"
+  "client.exe"
+  "client.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
